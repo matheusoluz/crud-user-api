e4808c88d6edab9d3b44aed9a41169514b54521f
MatheusLuz <matheus.luz12@gmail.com>
2019-03-06 21:32:51 -0300

    correção de autenticação

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,27 +1,8 @@
 'use strict';
 const debug = require('debug')('Autenticacao');
-const _ = require('lodash');
 
 module.exports = async (server) => {
   debug('enable authentication');
   server.enableAuth();
-
-  var Role = server.models.Role;
-
-  Role.registerResolver('user', ACLValidate(Role));
 };
 
-const ACLValidate = Role => async (role, ctx) => {
-  debug('Valida se o usuário esta logado');
-  const accessToken = ctx.remotingContext.req.accessToken;
-  if (!accessToken) return false;
-
-  debug('Valida se o usuário tem permissão de acesso');
-  return server.models.User.findById(accessToken.userId)
-    .then(Inst => {
-      if (Inst.username == 'admin') return callback(null, true);
-      const { methodString } = ctx.remotingContext;
-
-      return (Inst.userACL || []).includes(methodString);
-    });
-}
