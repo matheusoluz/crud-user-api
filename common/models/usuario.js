a996e62421571626ebf41ee800d763f6b02831ad
MatheusLuz <matheus.luz12@gmail.com>
2019-03-07 00:06:42 -0300

    método computado para retorno de permissão de usuário

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -1,5 +1,25 @@
 'use strict';
+const debug = require('debug')('Usuarios');
+const lodash = require('lodash');
 
-module.exports = function(Usuario) {
+module.exports = Usuario => {
+  Usuario.computeACL = async (item, ctx) => {
+    debug('Criando lista de permissões!');
+    if (!item.id) return undefined;
+    const query = require('../functions/query')(Usuario.app.datasources.db);
+    const result = lodash(await query(stmt, {principalId: item.id}))
+      .map(El => El.name)
+      .uniq()
+      .sort()
+      .value();
 
+    debug('computeACL: ', item.id, result);
+    return result[0];
+  };
 };
+
+const stmt = `SELECT R.name
+FROM RoleMapping AS RM
+INNER JOIN Role AS R ON RM.roleId = R.id
+WHERE (RM.principalType = 'USER')
+  AND (RM.principalId = @principalId)`;
