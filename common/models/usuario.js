f0a660522f4d694b57514a47a870460cca30825b
MatheusLuz <matheus.luz12@gmail.com>
2019-03-10 20:45:45 -0300

    Substituição native query e retorno de objeto role

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -3,23 +3,17 @@ const debug = require('debug')('Usuarios');
 const lodash = require('lodash');
 
 module.exports = Usuario => {
-  Usuario.computeACL = async (item, ctx) => {
+  Usuario.computeACL = async (item) => {
     debug('Criando lista de permissões!');
     if (!item.id) return undefined;
-    const query = require('../functions/query')(Usuario.app.datasources.db);
-    const result = lodash(await query(stmt, {principalId: item.id}))
-      .map(El => El.name)
-      .uniq()
-      .sort()
-      .value();
-
-    debug('computeACL: ', item.id, result);
-    return result[0];
+    const {Role, RoleMapping} = Usuario.app.models;
+    const roleMappingUser = await RoleMapping.findOne({
+      where: {
+        principalId: item.id,
+        principalType: 'USER',
+      },
+    });
+    if (roleMappingUser) return await Role.findById(roleMappingUser.roleId, {fields: ['id', 'name', 'description']});
+    else return undefined;
   };
 };
-
-const stmt = `SELECT R.name
-FROM RoleMapping AS RM
-INNER JOIN Role AS R ON RM.roleId = R.id
-WHERE (RM.principalType = 'USER')
-  AND (RM.principalId = @principalId)`;
